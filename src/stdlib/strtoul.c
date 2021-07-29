21d0c21f0e9e3e01e3c54cb96f97da69448c9005
Lance Hart <shipsimfan@hotmail.com>
2021-07-29 15:10:21 -0400

    Fixed bug in strtoul

--- src/stdlib/strtoul.c
+++ src/stdlib/strtoul.c
@@ -4,12 +4,10 @@ unsigned long int strtoul(const char* str, char** endptr, int base) {
     if (base < 2 || base > 36)
         return 0;
 
-    base--;
-
     unsigned long int value = 0;
     while (*str) {
-        if (base < 10) {
-            if (*str >= '0' && *str <= '0' + base) {
+        if (base <= 10) {
+            if (*str >= '0' && *str < '0' + base) {
                 value *= base;
                 value += *str - '0';
             } else
@@ -18,10 +16,10 @@ unsigned long int strtoul(const char* str, char** endptr, int base) {
             if (*str >= '0' && *str <= '9') {
                 value *= base;
                 value += *str - '0';
-            } else if (*str >= 'a' && *str <= 'a' + base - 10) {
+            } else if (*str >= 'a' && *str < 'a' + base - 10) {
                 value *= base;
                 value += *str - 'a' + 10;
-            } else if (*str >= 'A' && *str <= 'A' + base - 10) {
+            } else if (*str >= 'A' && *str < 'A' + base - 10) {
                 value *= base;
                 value += *str - 'A' + 10;
             } else
