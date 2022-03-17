663c55484c59496d89f0910fa56c62dd99ce4e80
Lance Hart <shipsimfan@hotmail.com>
2022-03-16 20:01:56 -0400

    Updated signal definitions

--- include/signal.h
+++ include/signal.h
@@ -1,13 +1,10 @@
 #ifndef __SIGNAL_H
 #define __SIGNAL_H
 
-#define SIGABRT 0
-#define SIGFPE 1
-#define SIGILL 2
+#define SIGKILL 0
+#define SIGTERM 1
+#define SIGABRT 2
 #define SIGINT 3
-#define SIGSEGV 4
-#define SIGTERM 5
-#define SIGKILL 6
 
 #define SIG_DFL ((void (*)(int))0)
 #define SIG_ERR ((void (*)(int)) - 1)
