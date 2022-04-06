0cc731c1725b90836e6b3948612f1edc8b0fa836
Lance Hart <shipsimfan@hotmail.com>
2022-04-05 23:50:00 -0400

    Removed math

--- src/math/log10.c
+++
@@ -1,3 +0,0 @@
-#include <math.h>
-
-double log10(double x) { return log(x) / 2.30258509299; }
--- src/math/math.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MATH_INT_H
-#define __MATH_INT_H
-
-#include <los/types.h>
-
-double __math_div_zero(u32 sign);
-double __math_invalid(double x);
-
-#endif
